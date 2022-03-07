5570e47cded6c68fe3eb03339e97e8038aad3e57
504 <504@DESKTOP-5GM5QKH>
2022-03-07 15:58:01 +0900

    Day05

--- java1/src/Day05/Day05_3.java
+++ java1/src/Day05/Day05_3.java
@@ -1,11 +1,105 @@
 package Day05;
 
+import java.util.Scanner;
+
 public class Day05_3 { //c s
 	public static void main(String[] args) {//m s
 		
+		//p.181
+			//1.
+			//2.
+			//3.2: 객체 비교시  equals 메소드 사용
+			//4.2: 길이가 없을 경우 값 대입 불가함.
+			//5.3: boolean 초기값은false
+			//6.3, 5 : {1행}, {2행}, {3행} =>3
+			//		   {78 83 93 87 88}=>5
+			//7.
+			int max=0;
+			int[]array= {1,5,3,8,2};
+			for(int i=0;i<array.length;i++) {
+				//i는  0부터 배열의 길이까지 1씩 증가 실행 반복 처리
+				if(max<array[i]) max=array[i];
+				//만약에 max보다 i번째 인덱스의 값이 더 크면 max에  i번째  값 입력
+			}
+			System.out.println("max : "+max);
+			//8.
+			int[][]array2= {
+					{95,86},
+					{83,92,96},	
+					{78,83,93,87,88}
+			};
+			
+			int sum=0; 		//합계
+			double avg=0.0; //평균
+			
+			int count =0;	//점수의 갯수[칸갯수]
+				//2차원 배열 내 모든 데이터호출
+			for(int 행=0;행<array2.length;행++) {	//행은 0부터 배열의 행길이[3]미만까지 1씩 증가
+				for(int 열=0;열<array2[행].length;열++) {//열은 0부터 배열의 열길이[2,3,5]미만까지 1씩 증가
+					sum+=array2[행][열];//누적합계
+				}
+				count +=array2[행].length;//열길이의 누적합계
+			}
+			
+			avg=(double)sum/count;
+			
+			System.out.println("sum: "+sum);
+			System.out.println("avg: "+avg);
 		
-		
-		
+			
+			//9.
+			boolean run=true;
+			int  studentNum=0;
+			int[] scores=null;
+			Scanner scanner=new Scanner(System.in);
+			
+			//while(true)
+			while(run) {
+				System.out.println("-----------------------------------");
+				System.out.println("1.학생수|2.점수입력|3.점수리스트|4.분석|5.종료");
+				System.out.println("-----------------------------------");
+				System.out.println("선택 >");
+				
+				int selentNo=scanner.nextInt();
+				if(selentNo==1) {
+					System.out.println("학생수> "); studentNum=scanner.nextInt();
+					//입력받은 학생 수 만큼 배열길이 선언
+					scores=new int[studentNum];
+				}
+				else if(selentNo==2) {//학생 점수 입력받기
+					for(int i=0;i<scores.length;i++) {//배열 내 모든 인덱스만큼 반복처리
+						System.out.printf("scores[%d] : ",i);
+						scores[i]=scanner.nextInt();//입력받아 i번째 인덱스에 저장
+					}//for end
+				}
+				else if(selentNo==3) {
+					for(int i=0;i<scores.length;i++) {//학생 점수 출력하기
+						System.out.printf("scores[%d] : %d\n",i,scores[i]);
+					}
+				}
+				else if(selentNo==4) {
+					int 최고점수=0;
+					int 총합계=0;
+					double 평균점수=0.0;
+					for(int i=0;i<scores.length;i++) {
+						//최고점수 구하기
+						if(최고점수<scores[i])최고점수=scores[i];//i번째 인덱스에 데이터 대입
+						//누적합계
+						총합계 +=scores[i];
+								
+					}
+					//평균점수구하기
+					평균점수=(double)총합계/scores.length;
+					
+					System.out.println("최고점수 : "+최고점수);
+					System.out.println("평균점수 : "+평균점수);
+				}
+				else if(selentNo==5) {
+					run=false;//break; 반복문 탈출
+				}//else if end
+			}//while end
+			System.out.println("프로그램 종료");
+			
 	}// m e
 
 }//c e
--- java1/src/Day05/Day05_4.java
+++ java1/src/Day05/Day05_4.java
@@ -1,11 +1,76 @@
 package Day05;
 
+import java.util.Scanner;
+
 public class Day05_4 { //c s
 	public static void main(String[] args) {//m s
+		//1.입출력  2.변수/자료형 3.제어/반복 4.제어/반복 5.배열
+		
+		//도서 대여 프로그램
+			//1.배열 변수
+				//1. 회원[아이디(중복X),비밀번호]
+				//2. 도서[도서명, 도서대여여부(대여가능,대여중), 대여인(ID)]
+			//2. 초기메뉴
+				//1. 회원가입 2.로그인
+					//1.회원가입시 아이디중복체크
+			//3. 로그인시메뉴
+				//1. 도서검색 2.도서목록 3.도서대여 4.도서반납 5.로그아웃
+					//1. 도서 검색시 도서명이 일치하면 도서명과 도서대여여부 출력,아니면 없음.
+					//2. 도서목록시 모든 도서명  출력
+					//3. 도서대여시 도서대여여부가 가능할 때 도서대여처리
+					//4. 도서 반납시  본인이  대여 한도서만 반납처리
+					//5. 로그아웃
+			//4. 로그인시 아이디가 admin이면 관리자메뉴
+				//1. 도서등록 2.도서목록 3.도서삭제 4.로그아웃
+					//1.도서 등록시 도서명을 입력받아 도서 등록 처리
+					//2.도서목록시 모든도서명 출력
+					//3.도서삭제시 삭제할 도서명을 입력받아 동일한도서명 삭제[null]
+					//4.로그아웃시 초기메뉴로
+		while(true) {
+		System.out.println("-------도서대여 관리자 모드--------");
+		System.out.println("1. 도서등록 2.도서목록 3.도서삭제 4.로그아웃");
+
+		Scanner scanner = new Scanner(System.in); int 선택 = scanner.nextInt();
+		String[] booklist=new String[100];
+		if (선택==1) {//if 1
+			System.out.println("-------도서 등록-------");
+			System.out.println("도서명을 입력하시오. : "); String 도서=scanner.next();
+			boolean 도서중복=true;
+			for(int i=0;i<booklist.length;i++) {
+				if(booklist[i] !=null&& booklist[i].equals(도서)) {//if 2
+					System.err.println("알림))동일한 책이 존재합니다.");
+					도서중복=false;
+					break;
+					}//if2 end
+			}//for end
+			if(도서중복) {
+				for(int i=0;i<booklist.length;i++) {
+					if(booklist[i]==null) {
+						booklist[i]=도서;
+					}
+				System.err.println("도서가 등록되었습니다.");
+				break;
+				}//for end
+				
+			}//if end
+		}//if1 end
+		else if (선택==2) {
+			for(int i=0;i<booklist.length;i++) {
+				if (booklist[i] !=null) {			
+				System.out.println("--------도서목록--------");
+				System.out.println("번호\t도서목록\t");
+				System.out.printf("%d\t%s",i,booklist[i]);
+					}
+			}
+		}//else 2 end
+		else if(선택==3) {}
+		else if(선택==4) {}
+		else {System.err.println("올바르지 못한 방법입니다.");}
 		
 		
 		
 		
+		}//while end
 	}// m e
 
 }//c e
