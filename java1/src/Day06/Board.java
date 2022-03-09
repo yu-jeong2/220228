d8edeb5800bcfb206c61004e97d34d05d3c108fa
ccs-22 <ccs-22@DESKTOP-D6R6Q9E>
2022-03-09 20:07:55 +0900

    Day06

---
+++ java1/src/Day06/Board.java
@@ -0,0 +1,17 @@
+package Day06;
+
+public class Board {
+
+	String title;
+	String content;
+	String wirst;
+	String pw;
+	
+	Board(String title, String content, String wirst, String pw){
+		this.title=title;
+		this.content=content;
+		this.wirst=wirst;
+		this.pw=pw;
+	}
+	
+}
